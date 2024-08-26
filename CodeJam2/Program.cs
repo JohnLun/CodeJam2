52d71cc4ca667d40b848de9d7464bfe8c44757c2
Jonathan Lun <jonathan.lun23@gmail.com>
2024-08-26 11:17:18 -0400

    Added comments

--- CodeJam2/Program.cs
+++ CodeJam2/Program.cs
@@ -19,20 +19,26 @@ namespace CodeJam2
             //initialize Game
             Console.WriteLine("How much are you playing with today?");
 
+            //initializing player, dealer, deck objects
             int cashStack = int.Parse(Console.ReadLine());
             Player player = new Player(cashStack);
             Dealer dealer = new Dealer();
             Deck deck = new Deck();
             Console.Clear();
 
-            //While player has enough money
+            //While player has enough money and chooses to keep playing
             while (player.CanPlay() && keepPlaying == "Y")
             {
+                //Printing messages
                 Console.WriteLine(FiggleFonts.Standard.Render("BLACKJACK"));
                 Console.WriteLine("Current Balance:");
                 Console.ForegroundColor = ConsoleColor.Green;
+
+                //Current Cash stack
                 Console.WriteLine(FiggleFonts.Standard.Render($"${player.GetCashStack()}"));
                 Console.ResetColor();
+
+                //Resetting Player and Dealer hands
                 player.FullReset();
                 dealer.FullReset();
                 List<int> playerHand = new List<int>();
@@ -47,18 +53,20 @@ namespace CodeJam2
                     wager = int.Parse(Console.ReadLine());
                 }
 
+                //Dealing out cards to player and dealer
                 int[] hands = deck.Deal();
                 player.Add(hands[0]);
-                
                 player.Add(hands[2]);
 
                 dealer.AddScore(hands[1]);
                 dealer.AddScore(hands[3]);
 
+                //Printing Dealer + Player hands to console
                 deck.PrintHand(dealer.GetHand());
                 Console.WriteLine("\n");
                 deck.PrintHand(player.GetHand());
 
+                //Keep dealing while player keeps hitting
                 string ans = "";
                 bool playerLost = false;
                 while (ans != "S")
@@ -73,6 +81,8 @@ namespace CodeJam2
                         deck.PrintHand(player.GetHand());
                         Console.WriteLine("\n");
                     }
+
+                    //Player loses if greater than 21, restarts hand
                     if(player.Bust())
                     {
                         Console.WriteLine("BUSTED!");
@@ -81,6 +91,8 @@ namespace CodeJam2
                     }
                 }
 
+
+                //If player lost, output how much player lost
                 if(playerLost)
                 {
                     Console.WriteLine("You Lost:\n");
@@ -94,18 +106,23 @@ namespace CodeJam2
                 }
                 bool dealerBust = false;
 
+                //Keep dealing to dealer while below score of 17
                 while (dealer.MustHit())
                 {
                     dealer.AddScore(deck.Hit());
                     deck.PrintHand(dealer.GetHand());
                     deck.PrintHand(player.GetHand());
                     Console.WriteLine("\n");
+
+                    //If dealer busts, player wins
                     if (dealer.Bust())
                     {
                         dealerBust = true;
                         break;
                     }
                 }
+
+                //Checking if player's score greater than dealer's score
 
                 if (player.GetTotScore() >= dealer.GetTotScore() || dealerBust)
                 {
@@ -121,6 +138,8 @@ namespace CodeJam2
                     Console.WriteLine($"-${wager}");
                     Console.ResetColor();
                 }
+
+                //Printing new cash stack
                 Console.WriteLine("New Balance:\n");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(FiggleFonts.Standard.Render($"${player.GetCashStack()}"));
@@ -131,7 +150,6 @@ namespace CodeJam2
                 deck = new Deck();
 
             }
-            Console.WriteLine("♥ ♦ ♣ ♠");
         }
         class Deck
         {
@@ -143,6 +161,7 @@ namespace CodeJam2
             }
             public void InitializeDeck()
             {
+                //Create a deck of cards
                 for(int i = 1; i < 14; i++)
                 {                   
                     deckOfCards.Add(i, 4);
@@ -151,6 +170,7 @@ namespace CodeJam2
 
             public int[] Deal()
             {
+                //Deal 4 cards, 2 to player, 2 to dealer
                 int[] hands = new int[4];
                 for(int i = 0; i < 4; i++)
                 {
@@ -161,6 +181,7 @@ namespace CodeJam2
 
             public int Hit()
             {
+                //Grabs a random card from the Deck and returns the value
                 Random rnd = new Random();
                 int randomCardVal = rnd.Next(2, 14);
                 while (deckOfCards[randomCardVal] == 0)
@@ -171,6 +192,7 @@ namespace CodeJam2
                 return randomCardVal;
             }
 
+            //Prints the hand
             public void PrintHand(List<int> hand)
             {
                 StringBuilder toRender = new StringBuilder();
@@ -198,6 +220,7 @@ namespace CodeJam2
                 Console.Write(FiggleFonts.Standard.Render(toRender.ToString()));
             }
 
+            //Prints the deck
             public void PrintDeck()
             {
                 foreach (var card in deckOfCards)
@@ -218,6 +241,7 @@ namespace CodeJam2
                 dealerHand = new List<int>();
             }
             
+            //Check if Bust
             public bool Bust()
             {
                 if (this.totScore > 21)
@@ -228,12 +252,14 @@ namespace CodeJam2
                 return false;
             }
 
+            //Reset hand
             public void FullReset()
             {
                 dealerHand = new List<int>();
                 totScore = 0;
             }
 
+            //Add Card value to current total score
             public void AddScore(int valOfCard)
             {
                 if (valOfCard == 1)
@@ -247,14 +273,19 @@ namespace CodeJam2
                 }
             }
 
+            //Check if dealer has to hit
             public bool MustHit()
             {
                 return totScore < 17;
             }
+
+            //Get dealer's current score
             public int GetTotScore()
             {
                 return this.totScore;
             }
+
+            //Get dealer's current hand
             public List<int> GetHand()
             {
                 return dealerHand;
@@ -274,10 +305,13 @@ namespace CodeJam2
                 this.currWager = 0;
                 this.totScore = 0;
             }
+            //Check if Player has enough money
             public bool CanPlay()
             {
                 return cashStack > 0;
             }
+
+            //Check if Player can wager 
             public bool CanWager(int wager)
             {
                 if (wager > cashStack)
@@ -289,32 +323,39 @@ namespace CodeJam2
                 return true;
             }
 
-
+            //Get player's current cash stack
             public int GetCashStack()
             {
                 return this.cashStack;
             }
+
+            //Reset player hand
             public void FullReset()
             {
                 playerHand = new List<int>();
                 totScore = 0;
             }
+
+            //Get player's current hand value
             public int GetTotScore()
             {
                 return this.totScore;
             }
-
+            
+            //Add winning wager to player's cash stack
             public void Win()
             {
                 this.cashStack += currWager * 2;
             }
 
+            //Add value of card to player's total score
             public void Add(int valOfCard)
             {
                 this.totScore += Math.Min(10, valOfCard);
                 playerHand.Add(valOfCard);
             }
 
+            //Check if player busts
             public bool Bust()
             {
                 if(this.totScore > 21)
